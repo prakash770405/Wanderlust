e07c17a5ae472f9a577b5aa0eec71a6da0e2149c
prakash770405 <suryazzz642@gmail.com>
2025-11-15 14:58:38 -0500

    error handling features added through error.ejs and from utils folder

--- app.js
+++ app.js
@@ -1,114 +1,112 @@
-const express= require("express")
-const mongoose=require("mongoose");
-const Listing=require("./models/listing.js")      //fetching listing schema for database..
-const Review=require("./models/review.js")       //fetching review schema for database..
-const path=require("path");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-const review = require("./models/review.js");
-const app= express();
+const express = require("express")
+const mongoose = require("mongoose");
+const app = express();
+const Listing = require("./models/listing.js")      //fetching listing schema for database..
+const Review = require("./models/review.js")       //fetching review schema for database..
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
-app.engine("ejs",ejsMate);
 
-app.use(express.static(path.join(__dirname,"/public")));
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+
+app.engine("ejs", ejsMate);
+
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}));
-app.set("view engine","ejs");
-app.set(path.join(__dirname,"views"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 main()
-.then(()=>{console.log("connected to database");})
-.catch((err) => {console.log(err);});
+  .then(() => { console.log("connected to database"); })
+  .catch((err) => { console.log(err); });
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
-app.listen(8080,(req,res)=>{
-    console.log("app is listening on port 8080");
-})
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
-app.get("/",(req,res)=>{
-    res.send("working");
-})
-
-
-//---------------------------data inserting
-// app.get("/testlisting",async (req,res)=>{
-//     let sampleListing= new Listing({
-//         title:"hellobeach",
-//         description:"by the beach",
-//         price:1200,
-//         location:"goa",
-//         country:"india",
-//     });
-//      await sampleListing.save()
-//      console.log("saved");
-//      res.send("successful");
-//     .then(()=>{console.log("saved");})
-//     .catch((err)=>{console.log(err);})
-// })
-
-//----------------------------------------new listing
-app.get("/listings/new",(req,res)=>{
+//----------------------------------------new listing (create new list)
+app.get("/listings/new", (req, res) => {
   res.render("./listings/new.ejs");
-})
+});
 
-app.post("/listings",async(req,res)=>{
- 
-  // let {title,description,price,country,location}=req.body;  //------pahla tarika req ki body se data lenae ka
-  // console.log(title,description,price,country,location);
-  //let listing=req.body.listing; //-------dossara tarika ye hai ki form k name ko variable k sath likh do
-  //console.log(listing);
-
-
-  const newListing= new Listing(req.body.listing); //<------kuch iss tarah------>
+app.post("/listings", wrapAsync(async (req, res, next) => {
+  if (!req.body.listing) {
+    throw new ExpressError(400, "Send valid data for listings");
+  }
+  const newListing = new Listing(req.body.listing); //<------kuch iss tarah------>
   await newListing.save();
   res.redirect("/listings");
-
-})
+}));
 
 //------------------------------------------------listing index route
-app.get("/listings",async(req,res)=>{
-const allListing= await Listing.find({});
-res.render("./listings/index.ejs",{allListing});
-})
+app.get("/listings", wrapAsync(async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("./listings/index.ejs", { allListing });
+}));
 //----------------------------------------------show route
-app.get("/listings/:id",async(req,res)=>{
-  let {id}=req.params;
-  const listing=await Listing.findById(id).populate("reviews");
-res.render("./listings/show.ejs",{listing});
-})
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id).populate("reviews");
+  res.render("./listings/show.ejs", { listing });
+}));
 
 //---------------------------------------------edit route
 
-app.get("/listings/:id/edit",async(req,res)=>{
-   let {id}=req.params;
-  const listing=await Listing.findById(id);
-  res.render("./listings/edit.ejs",{listing});
-})
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/edit.ejs", { listing });
+}));
 
-app.put("/listings/:id" ,async (req,res)=>{
-let {id}=req.params;
-  
-await Listing.findByIdAndUpdate(id,{...req.body.listing});
-res.redirect(`/listings/${id}`);
-})
+//---------------------------------------------update route
+
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+   if (!req.body.listing) {
+    throw new ExpressError(400, "Send valid data for listings");
+  }
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+}));
 
 //-----------------------------------delete route
-app.delete("/listings/:id",async(req,res)=>{
-  let {id}=req.params;
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+  let { id } = req.params;
   await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
-})
+}));
 
-//-------------------------------------reviews route
-app.post("/listings/:id/reviews",async(req,res)=>{
-  let {id}=req.params;
- let listing= await Listing.findById(id)
- let newReview=new Review(req.body.review)
- listing.reviews.push(newReview);
+//------------------------------------- create new reviews route
+app.post("/listings/:id/reviews", wrapAsync(async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id)
+  let newReview = new Review(req.body.review)
+  listing.reviews.push(newReview);
   await newReview.save();
   await listing.save();
   res.redirect(`/listings/${listing._id}`);
-})
+}));
+
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found")); // for random undefined routes its says page not found
+});
+
+
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;//if something unknown happens than the exact errror will be displayed
+  res.status(statusCode).render("./listings/error.ejs", { message});//like validation error failed due to casting
+  // res.status(statusCode).send(message);
+});
+
+
+app.listen(8080, (req, res) => {
+  console.log(`app is listening on port 8080`);
+});
